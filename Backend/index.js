c954aa53fbee6a9374c47fc8466a5b360cfac4d2
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-07-30 19:02:05 +0530

    main

--- Backend/index.js
+++ Backend/index.js
@@ -2,8 +2,23 @@ import express from 'express';
 
 const app = express();
 
+app.use(function(req, res, next) {
+    console.log('Ready to serve');
+    next();
+})
+
+app.use(function(req, res, next) {
+    console.log('connecting ho raha h');
+    next();
+})
+
 app.get("/", (req, res)=>{
     res.send("Our Backend is Properly Work!")
+})
+
+app.use(function(req, res, next) {
+    console.log('Ab hum features wale route pe jaa rhe h');
+    next();
 })
 
 app.get("/features", (req, res)=>{
