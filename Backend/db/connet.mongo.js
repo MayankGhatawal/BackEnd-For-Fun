d608b1ff4e985ac923557c1615b6b9f15b281c03
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-07-30 18:43:32 +0530

    main

--- Backend/db/connet.mongo.js
+++ Backend/db/connet.mongo.js
@@ -1,17 +1,28 @@
 import mongoose from "mongoose";
 import express from "express";
-import USE_NAME from "../controller/ui.new.js"
+import {USE_NAME} from "../controller/ui.new.js"
+import dotenv from "dotenv";
+
+dotenv.config(
+    {
+        path: "../env"
+    }
+);
 
 const app = express();
 
 (async ()=>{
     try {
         await mongoose.connect(`${process.env.MONGO_URL}/${USE_NAME}`)
-        app.on('/', function(req, res){
-            console.log("MongoDB Connected Successfully !");
+        app.on('error', (err)=>{
+            console.error("ERROR ",err);
+            process.exit(1);
+        });
+        app.listen(3000, function(){
+            console.log("MongoDB Connected Successfully!");
         })
     } catch (error) {
-        console.log("Error: " + error);
+        console.log("Error: ", error);
         process.exit(1);
     }
 })()
