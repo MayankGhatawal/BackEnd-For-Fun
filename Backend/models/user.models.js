9383068fb7b458a0b847d484766995da370f347a
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-07-30 18:15:06 +0530

    main

---
+++ Backend/models/user.models.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["admin", "user"],
+        default: "user"
+    }
+}, {timestamps: true});
+
+const User = mongoose.model("User", userSchema);
