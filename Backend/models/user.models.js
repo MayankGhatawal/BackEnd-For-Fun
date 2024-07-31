4377f1eea01199ddb55c2b4076fe62c4d47bf35b
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-07-31 09:59:20 +0530

    main

--- Backend/models/user.models.js
+++ Backend/models/user.models.js
@@ -1,24 +1,60 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-    name: {
+const userSchema = new Schema(
+{
+    username: {
         type: String,
-        required: true
+        required: true,
+        minlength: 3,
+        maxlength: 20,
+        lowercase: true,
+        unique: true,
+        trim: true,
+        index: true
     },
     email: {
         type: String,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true,
+        trim: true,
+        index: true
     },
-    password: {
+    fullname: {
+        type: String,
+        required: true,
+        trim: true,
+        index: true,
+    },
+    avatar:{
         type: String,
         required: true
     },
-    role: {
+    coverimage:{
         type: String,
-        enum: ["admin", "user"],
-        default: "user"
-    }
-}, {timestamps: true});
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8,
+        select: false,
+        validate: {
+            validator: (password) => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(password),
+            message: "Password must be at least 8 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+        }
 
-const User = mongoose.model("User", userSchema);
+    },
+    watchHistory: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    refreshToken: {
+        type: String,
+        select: false,
+    },
+
+},{timestamps: true});
+
+export const User = mongoose.model("User", userSchema);
