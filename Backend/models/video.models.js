e62b2a6f7d03ffa5b44288f786f18e41f278c7cf
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-07-31 19:55:12 +0530

    main

--- Backend/models/video.models.js
+++ Backend/models/video.models.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
+
 const videoSchema = new Schema({
     title:{
         type: String,
@@ -41,5 +42,4 @@ const videoSchema = new Schema({
 },{timestamps: true});
 
 videoSchema.plugin(mongooseAggregatePaginate)
-
 export const Video = mongoose.model("Video", videoSchema);
