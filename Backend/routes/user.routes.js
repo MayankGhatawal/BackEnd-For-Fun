2a94cb947124e0ab4ff65cd626cbeeeb734d7b13
MayankGhatawal <mayank1ghatawal@gmail.com>
2024-08-02 11:13:22 +0530

    main

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,6 +1,8 @@
 import { Routes } from 'express';
+import { userRegistration } from '..controller/user.constroller.js';
 
 const router = Routes();
 
+router.route('/register').post(userRegistration);
 
 export default router
